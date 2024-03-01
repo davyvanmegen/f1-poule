32b2d6ecc671d4bc8d05bbf832364a5c50a7dcfc
Simon Damminga <simon.damminga@gmail.com>
2024-03-01 19:18:09 +0100

    deploy steps

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:davyvanmegen/f1-poule.git main:gh-pages
+git push -f git@github.com:davyvanmegen/f1-poule.git master:gh-pages
 
 cd -
